d5660f08a7f249d8423d2c097bbf19d1e18a1539
Andrew Urom <andrewurom@gmail.com>
2020-03-24 21:24:23 +0100

    Mocha is working

---
+++ test/main_test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+//Test Suite
+describe('Mocha', () => {
+    // Unit Testing
+    it('Should run our test using npm', ()=> {
+        expect(true).to.be.ok;
+    })
+})
